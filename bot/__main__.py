479d8f538c95e78b5fe59fdd8cf8d6bb1c0ea173
Matthias Wijnsma <matthiasx95@gmail.com>
2022-08-31 21:38:49 +0200

    Revert "Added commandline argument for debug mode"
    This reverts commit b7ce17a55e017cd1717cc488f5ee155461e94ce4.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,17 +1,11 @@
-import argparse
 import discord
 from bot.constants.discord import bot as constants
 from bot.managers.manager import Manager
 
-parser = argparse.ArgumentParser()
-parser.add_argument("token", type=str, help="The token to run the bot with.")
-parser.add_argument("-d", "--debug", action="store_true", help="Enables debug mode.")
-args = parser.parse_args()
-
 intents = discord.Intents.default()
 
 bot = discord.Bot(
-    debug_guilds=[constants.DEBUG_GUILD if args.debug else constants.PRODUCTION_GUILD],
+    debug_guilds=constants.DEBUG_GUILDS,
     intents=intents,
     owner_id=constants.OWNER_ID,
 )
@@ -22,11 +16,11 @@ manager = Manager()
 # TODO: Get rid of this, it's just for debugging
 @bot.event
 async def on_ready() -> None:
-    print(f"Running bot as {bot.user} in {'debug' if args.debug else 'release'} mode")
+    print(f"Logged in as {bot.user}")
 
 
 for cog in constants.COGS:
     bot.add_cog(cog(bot=bot, manager=manager))
 
 
-bot.run(args.token)
+bot.run(constants.TOKEN)
--- bot/constants/discord/bot.py
+++ bot/constants/discord/bot.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List, Type
 from bot.cogs.base import BaseCog
 from bot.cogs.listeners import ListenersCog
@@ -6,10 +7,13 @@ from bot.cogs.utility import UtilityCog
 
 OWNER_ID = 789145924235821087
 
-DEBUG_GUILD = 967178834984202340
-PRODUCTION_GUILD = 841473212763734027
+DEBUG_GUILDS = [
+    841473212763734027,
+]
 
 COGS: List[Type[BaseCog]] = [
     ListenersCog,
     UtilityCog,
 ]
+
+TOKEN = sys.argv[1]
